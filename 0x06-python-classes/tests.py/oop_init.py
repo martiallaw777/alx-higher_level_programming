8aab50a43eea7714d9e77facff21b0c3f8e133b7
martiallaw777 <100254945+martiallaw777@users.noreply.github.com>
2022-06-22 00:30:54 +0100

    remove

--- 0x06-python-classes/tests.py/oop_init.py
+++
@@ -1,6 +0,0 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
-    def say_hi(self):
-        print('Hello, my name is Florence Nyokabi Wangui', self.name)
-Person('Florence Nyokabi Wangui').say_hi()
--- 0x06-python-classes/tests.py/oop_method.py
+++
@@ -1,4 +0,0 @@
-class Person:
-    def say_hi(self):
-        print('Hello, how are you?')
-Person().say_hi()
